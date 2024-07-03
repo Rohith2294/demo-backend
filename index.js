9617e73d5a0d411bd482d5fb9365f309035d84f9
Rohith <rohith.pasupuleti@visiram.com>
2024-07-03 13:19:56 +0530

    second commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+// nsnss
 // CORS Headers Middleware
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
